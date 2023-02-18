d581b02632ddb1a156ed3d380b8bd2f89dcd4444
mikey1 <mikey1997@gmail.com>
2023-02-18 14:10:20 +0530

    Added database

--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami           = ami-0557a15b87f6559cf
+  ami           = "ami-0557a15b87f6559cf"
   instance_type = "t2.micro"
   associate_public_ip_address = true
   availability_zone = "us-east-1a"
