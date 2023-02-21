d4ce4b367df11709956be0bd64d64385b8b5a4af
mikey1 <mikey1997@gmail.com>
2023-02-21 15:03:23 +0530

    Added changes

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ module "vpc" {
   private_subnets = var.azs_details.private_subnets
   public_subnets  = var.azs_details.public_subnets
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
 
   tags = {
     Terraform = "true"
