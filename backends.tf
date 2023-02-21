d5a447b571ff5b3dacafd1e0c2e5f5f27d394a1d
mikey1 <mikey1997@gmail.com>
2023-02-21 21:52:21 +0530

    Added changes

--- backends.tf
+++ backends.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {
         bucket = "mikey12345"
         key = "mikeydev"
-        dynamodb_table = "mikeydb"
         region = "us-east-1"
+        dynamodb_table = "mikeydb"
     }
 }
